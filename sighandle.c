466502baf1c8fcbbfb3204c8115dca43a629fec1
sidb596 <54995684+sidb596@users.noreply.github.com>
2019-10-15 23:57:24 -0500

    commit2

--- sighandle.c
+++ sighandle.c
@@ -2,23 +2,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void sigint_handler(int sig){
-  char msg[] = "Signal handled.  Deal with it.\n";
-  write(1, msg, sizeof(msg));
-  exit(0);
+#define SIGINT 1   // Interrupt the process
+#define SIGTSTP 2  // Stop the process
+
+void sigint_handler(int sig) {
+  int n = sig;
+  char *msg;
+  switch (n) {
+    case 1:  // code to be executed if n = 1;
+      msg = "SIGINT Handled\n";
+      write(1, msg, sizeof(msg));
+      exit(0);
+      break;
+    case 2:  // code to be executed if n = 2;
+      msg = "SIGTSTP Handled\n";
+      write(1, msg, sizeof(msg));
+      exit(0);
+      break;
+      //  default:  // code to be executed if n doesn't match any cases
+  }
 }
 
-int main(){
+int main() {
   signal(SIGINT, sigint_handler);
 
   int i = 99;
-  while(1){
+  while (1) {
     printf("%d bottles of beer on the wall\n", i);
     printf("%d bottles of beer\n", i);
     printf("Take one down, pass it around\n");
     i = i - 1;
     printf("%d bottles of beer on the wall\n", i);
   }
-
-
 }
